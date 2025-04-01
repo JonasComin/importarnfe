4a6ae3aad34bee5205cdf50ffa499f1d176b87e9
JonasComin <comin@uol.com.br>
2025-04-01 01:11:00 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def criar_banco():
     cur = conn.cursor()
 
     # Tabela de Notas
-    cur.execute('''CREATE TABLE IF NOT EXISTS notas (
+    cur.execute("""CREATE TABLE IF NOT EXISTS notas (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         numero_nf TEXT,
         serie TEXT,
@@ -21,10 +21,10 @@ def criar_banco():
         destinatario TEXT,
         valor_total REAL,
         data_emissao TEXT
-    )''')
+    )""")
 
     # Tabela de Itens
-    cur.execute('''CREATE TABLE IF NOT EXISTS itens (
+    cur.execute("""CREATE TABLE IF NOT EXISTS itens (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nota_id INTEGER,
         descricao TEXT,
@@ -34,15 +34,15 @@ def criar_banco():
         ncm TEXT,
         cst TEXT,
         FOREIGN KEY (nota_id) REFERENCES notas(id)
-    )''')
+    )""")
 
     # Tabela de NCM
-    cur.execute('''CREATE TABLE IF NOT EXISTS ncm_cadastro (
+    cur.execute("""CREATE TABLE IF NOT EXISTS ncm_cadastro (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         ncm TEXT UNIQUE,
         substituicao_tributaria TEXT,
         data_atualizacao TEXT
-    )''')
+    )""")
 
     conn.commit()
     conn.close()
@@ -50,6 +50,26 @@ def criar_banco():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/importar_xml")
+def importar_xml():
+    return render_template("importar.html")
+
+@app.route("/ver_ncm")
+def ver_ncm():
+    return render_template("ncm.html")
+
+@app.route("/notas")
+def ver_notas():
+    return render_template("notas.html")
+
+@app.route("/itens")
+def ver_itens():
+    return render_template("itens.html")
+
+@app.route("/editar_ncm/<int:id>")
+def editar_ncm(id):
+    return render_template("editar_ncm.html", id=id)
 
 if __name__ == "__main__":
     criar_banco()
